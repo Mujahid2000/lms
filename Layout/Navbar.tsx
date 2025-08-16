27399094f34374bcec300f8744ec4b3525d41936
Mujahidul Islam <developermujahid2001@gmail.com>
2025-08-16 19:24:52 +0600

    uppercase

--- Layout/Navbar.tsx
+++ Layout/Navbar.tsx
@@ -46,7 +46,7 @@ useEffect(() =>{
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-4">
             <div>
-              <h1 className="text-2xl font-bold text-gray-900">LMS {role} Dashboard</h1>
+              <h1 className="text-2xl font-bold text-gray-900 uppercase">LMS {role} Dashboard</h1>
               <p className="text-sm text-gray-600">Welcome back, {role}</p>
             </div>
             {/* Desktop Navigation (visible on md: 768px and above) */}
